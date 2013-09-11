bc2e882f1793875b0f9766a2f31661052889a1a9
Arthur R. Wuterich <arthurwut@gmail.com>
2013-09-11 09:50:32 -0700

    Added legacy system to pwapi: Commands can be flagged as legacy and redirect to other commands. There is a constant in pwapi.cfg.php that will enable and disable these redirects

--- api/api.php
+++ api/api.php
@@ -3,23 +3,16 @@
 // Arthur Wuterich
 // 9-3-2013
 
-// System Constants
+// Paths
 define( 'LIB_PATH', 'lib/' );
 define( 'CMD_PATH', 'command/' );
-
-// Messahes
-define( 'ERROR_INVALID_POST', 'Post array or elements are impropertly formed' );
-define( 'ERROR_INVALID_KEY', 'API key is incorrect' );
-define( 'ERROR_INVALID_TASK', 'API task does not exist' );
-define( 'ERROR_VARIABLE_NOT_FOUND', 'A required variable was not found in the command argument array' );
 
 // Load api libraries
 require_once LIB_PATH . 'database/database.functions.php';
 require_once LIB_PATH . 'pwapi.functions.php';
 
-// System Variables
-// Returns an array of the commands available in the LIB_PATH/CMD_PATH/ directory
-$API_COMMANDS = GetCommandArray( scandir( LIB_PATH . CMD_PATH ) );
+// Load config file
+require_once LIB_PATH . 'pwapi.cfg.php';
 
 // Application Variables
 DEFINE( 'APIKEY', $_POST['apikey'] );
@@ -32,7 +25,6 @@ if( !APIKEY || !APITASK )
 	exit( ERROR_INVALID_POST );
 }
 // -Api key is incorrect
-// small hack to allow 'test' to work
 if( APIKEY != $_SERVER['API_APIKEY'] && APIKEY != 'test' )
 {
 	exit( ERROR_INVALID_KEY );
@@ -44,8 +36,17 @@ if( !in_array( $_POST['apitask'], $API_COMMANDS ) )
 	exit( ERROR_INVALID_TASK );
 }
 
-// Execute command script which is expected to output the required data
-include( LIB_PATH . CMD_PATH . APITASK . '.php' );
+$Apitask = APITASK;
+
+// Check if the Apitask is a legacy task
+// -Redirect to modern command if Apitask is legacy and the redirect command exists
+if( !LEGACY && isset( $LEGACY_COMMANDS[APITASK] ) && in_array( $LEGACY_COMMANDS[APITASK], $API_COMMANDS ) )
+{
+	$Apitask = $LEGACY_COMMANDS[APITASK];
+}
+
+// Execute command
+include( LIB_PATH . CMD_PATH . $Apitask . '.php' );
 
 exit();
 
