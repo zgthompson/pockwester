273eb4c46451dcf3101426585066abc021a48fa5
Arthur R Wuterich <arthurwut@gmail.com>
2013-09-19 11:18:42 -0700

    Changed text in user help page

--- web/src/help_user.php
+++ web/src/help_user.php
@@ -5,7 +5,7 @@
 ?>
 <div id="login_window" class="rounded_window center_on_page small_window drop_shadow">
 	<h1> Pockwester Scheduling Application </h1>
-	<h2> I'll email you your password... </h2>
+	<h2> I'll email your password... </h2>
 	<form action="index.php" method="POST">
 		<label for="email">Email</label>
 		<input type="textfield" name="email"><BR/>
