e3eaab4a30ae9398c86bebcaaacb1bcaae7ec65c
Arthur R. Wuterich <arthurwut@gmail.com>
2013-09-27 23:06:07 -0700

    Added group details page for managing groups

--- web/src/manage_groups.php
+++ web/src/manage_groups.php
@@ -3,13 +3,6 @@
 // 9-17-13
 // Arthur Wuterich
 
-// Retuns a member name in a member block
-// Precondition: Valid string name
-function GroupMemberBlock( $name )
-{
-	return "<div class=\"member_block\">{$name}</div>";
-}
-
 // Takes a groupname and returns a group block
 // Precondition: valid group name string
 function CreateGroupBlock( $groupName )
@@ -17,7 +10,10 @@ function CreateGroupBlock( $groupName )
 	$html = 
 	"
 	<div class=\"group_block\">
-	<button name=\"this\" value=\"{$groupName}\">{$groupName}</button>
+	<form method=\"POST\">
+		<input type=\"hidden\" name=\"group_name\" value=\"{$groupName}\" />
+		<button name=\"goto\" value=\"group.php\">{$groupName}</button>
+	</form>
 	</div>
 	";	
 	
@@ -27,41 +23,18 @@ function CreateGroupBlock( $groupName )
 
 $outputHtml = '';
 
-// Get information about individual groups
-if( isset($_POST['this']) && $_POST['this'] != '' )
+// Get the groups this user is part of
+$post = array( 'user' => $_SESSION['USER_ID'] );
+$groups = json_decode( PWTask( 'get_groups', $post ) );
+
+if( is_array( $groups ) )
 {
-	// Get members in the group
-	$post = array( 'group' => $_POST['this'] );
-	$groupMembers = PWTask( 'get_users', $post );
-	$groupMembers = json_decode( $groupMembers ); 
-	
-	$outputHtml = 
-	"
-	<h1>{$_POST['this']}</h1>
-	<h2>Users part of group:</h2>";
-	
-	// Format them into a list
-	foreach( $groupMembers as $member )
+	foreach( $groups as $group )
 	{
-		$outputHtml .= GroupMemberBlock( $member );
-	}
-	
-	$outputHtml .= "<button name=\"this\" value=\"\">All Groups</button>";
-}
-else
-{
-	// Get the groups this user is part of
-	$post = array( 'user' => $_SESSION['USER_ID'] );
-	$groups = json_decode( PWTask( 'get_groups', $post ) );
-	
-	if( is_array( $groups ) )
-	{
-		foreach( $groups as $group )
-		{
-			$outputHtml .= CreateGroupBlock( $group[0] );
-		}
+		$outputHtml .= CreateGroupBlock( $group[0] );
 	}
 }
+
 ?>
 
 <div class="window_background center_on_page large_window drop_shadow">
